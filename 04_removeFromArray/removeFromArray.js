e7273c3264cee0e9340ae09d28c7db57555f123d
griffinpuncher <benjaminadams41@gmail.com>
2023-04-19 15:25:11 -0400

    Complete exercise 04-removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removeFromArray) {
+    
+    for (var i = 0; i < removeFromArray.length; i++) {
+        if (array.indexOf(removeFromArray[i]) !== -1) {
+            let indexToRemove = array.indexOf(removeFromArray[i]);
+            array.splice(indexToRemove,1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
