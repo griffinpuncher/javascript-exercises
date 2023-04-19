a2633f4d517323cd2a5bfa343dda6afd4f6a6e81
griffinpuncher <benjaminadams41@gmail.com>
2023-04-19 15:34:47 -0400

    Complete exercise 05-sumAll.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
+    let sum = 0;
+    
+    if (start < 0 || end < 0 || typeof start !== 'number' || typeof end !== 'number') {
+        return "ERROR";
+    }
+    else if (start > end) {
+        [start,end] = [end,start];
+    }
 
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
