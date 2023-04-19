9749f7d7699e92a2ebc000eeb16da17868643f3f
griffinpuncher <benjaminadams41@gmail.com>
2023-04-19 16:50:34 -0400

    Complete exercise 08-calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(...numberToAdd) {
+	return numberToAdd[0] + numberToAdd[1];
 };
 
-const subtract = function() {
-	
+const subtract = function(...numberToSubtract) {
+  return numberToSubtract[0] - numberToSubtract[1];
 };
 
-const sum = function() {
-	
+const sum = function(...numbersToSum) {
+  let sum = 0;
+  numbersToSum.flat().forEach(function(number) {sum += number;});
+	return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(...numbersToMultiply) {
+  let answer = 1;
+  numbersToMultiply.flat().forEach(number => {answer *= number});
+  return answer;
 };
 
-const power = function() {
-	
+const power = function(...numbersToPower) {
+	return numbersToPower[0]**numbersToPower[1];
 };
 
-const factorial = function() {
-	
+const factorial = function(factorial) {
+  let answer = 1;
+  if (factorial === 0) {return answer;};
+	for (let i = 1; i <= factorial; i++) {
+    answer = answer*i;
+  }
+  return answer;
 };
 
 // Do not edit below this line
