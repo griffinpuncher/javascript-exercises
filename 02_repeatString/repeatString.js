577b035fe7ca76889d16ac6a5c9e8f43392d0abb
griffinpuncher <benjaminadams41@gmail.com>
2023-04-19 14:27:12 -0400

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let newString = "";
+    for (var i = 0; i < num; i++) {
+        newString = newString + string;
+    }
 
+    if (num < 0) {
+        return "ERROR";
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
