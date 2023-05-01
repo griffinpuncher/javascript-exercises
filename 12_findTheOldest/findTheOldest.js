c6f008899ea493c4535ad17e6215b5bd8f2a9b42
griffinpuncher <benjaminadams41@gmail.com>
2023-05-01 01:26:04 -0400

    Solve exercise 12 findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        if (person.yearOfDeath == undefined) {
+            person.age = (new Date()).getFullYear() - person.yearOfBirth;
+        }
+        else{
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        }
+        if(!oldest) {
+            return person;
+        }
+        else {            
+            if (person.age > oldest.age) {
+                return person;
+            }
+            else {
+                return oldest;
+            }
+        }
+    },null)
 };
 
 // Do not edit below this line
