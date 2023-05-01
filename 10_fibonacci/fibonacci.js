d8f5848ab86c4e6cf8665d20fe88f26ef6d597a9
griffinpuncher <benjaminadams41@gmail.com>
2023-05-01 00:53:13 -0400

    Solve exercise 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    const number = parseInt(input);
+    let answer = 1;
+    let lastAnswer = 0;
+    if (number < 0) {return "OOPS"};
+    for (i = 0; i < number-1; i++) {
+        answer += lastAnswer;
+        lastAnswer = answer - lastAnswer;
+    }
+    return answer;
+    
 };
 
 // Do not edit below this line
