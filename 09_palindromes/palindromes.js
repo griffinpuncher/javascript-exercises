85faf778f969db20537c8378e10db0436d2530b3
griffinpuncher <benjaminadams41@gmail.com>
2023-04-30 21:41:21 -0400

    Solve exercise 09 Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (rawWord) {
+    word = rawWord.replace(/[\p{P}\s]/gu,"").toLowerCase();
+    const halfWay = Math.floor(word.length/2);
+    for(i = 0; i < halfWay; i++) {
+        if(word[i] !== word[word.length-1-i]) {return false}
+    }
+    return true;
 };
 
 // Do not edit below this line
